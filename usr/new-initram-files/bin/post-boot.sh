095b2f683b4a4606ac7eb24e1fd4f8039e671ee7
Eric Clothier <eric.clothier@yahoo.com>
2011-10-25 14:25:36 -0400

    git: fix gitignore some more
     - ignore releasetools/boot.img since it is regenerated every build
     - ignore usr/new-initram-files/* as they are unused and unneccesary, all initram changes
         will be reflected in usr/initram-files. I only keep new-initram-files to try out
         different setups
     - add a .gitignore file to each of usr/initram-files/data, dev, hidden, proc, sys, & system
         to make sure that those folders are copied to remote repos, since they are required for
         proper initram generation

--- usr/new-initram-files/bin/post-boot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/bin/sh /bin/run-parts.sh /bin/post-boot.d
-/bin/sh /bin/run-parts.sh /system/etc/init.d
--- usr/new-initram-files/bin/run-parts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PATH=/system/bin:/sbin:/system/xbin:/bin
-log "Executing run-parts on $*"
-logwrapper /bin/run-parts $*
